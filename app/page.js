3618b75170f2c734eb1794683a11a0a02055864e
anik475 <anikde475@gmail.com>
2024-10-06 11:08:08 -0400

    Fixed the toggle tag in the page layout

--- app/page.js
+++ app/page.js
@@ -91,7 +91,7 @@ export default function Home() {
                         <a href="mailto:anikde475@gmail.com" className="button">Contact Me</a> 
                     </li>
                 </ul>
-                <a href="#" className="mobile-toggle" onclick={toggleMobileMenu}>
+                <a href="#" className="mobile-toggle" onClick={toggleMobileMenu}>
                     <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                       <path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M5 7h14M5 12h14M5 17h10" />
                     </svg>
